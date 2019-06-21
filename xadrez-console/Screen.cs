41c6fcae8de33e2000907a596fadc3b72e6b71b5
Alef Duarte <alef.duarte@outlook.com>
2019-06-21 20:06:03 -0300

    enhanced print board

--- xadrez-console/Screen.cs
+++ xadrez-console/Screen.cs
@@ -9,6 +9,7 @@ namespace xadrez_console
         {
             for (int i = 0; i < board.Rows; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < board.Columns; j++)
                 {
                     if (board.Piece(i, j) == null)
@@ -17,10 +18,30 @@ namespace xadrez_console
                     }
                     else
                     {
-                        Console.Write(board.Piece(i, j) + " ");
+                        PrintPiece(board.Piece(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  A  B  C  D  E  F  G  H");
+        }
+
+        public static void PrintPiece(Piece piece)
+        {
+            if (piece.Color == Color.White)
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(piece);
+                Console.ForegroundColor = aux;
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(piece);
+                Console.ForegroundColor = aux;
             }
         }
     }
